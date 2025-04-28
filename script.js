26383eec05bdc2d610ed959ac5472fa2b9e9ea22
Amina Mehwish <157866327+AminaMehwishkhan@users.noreply.github.com>
2025-04-28 06:43:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let weight = parseFloat(prompt("Enter your weight in kg:"));
+let height = parseFloat(prompt("Enter your height in meters:"));
+
+let bmi = weight / (height * height);
+
+alert("Your BMI is: " + bmi);
